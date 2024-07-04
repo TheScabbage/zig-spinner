2827383c543c0b1f04f7dbcf5b6c5e7adcffb4ee
scabbage <thescabbage@gmail.com>
2024-07-04 21:05:17 +0800

    Add triple dot spinner

--- src/main.zig
+++ src/main.zig
@@ -1,25 +1,37 @@
 const std = @import("std");
 
 pub fn main() !void {
-    const frames = [12][]const u8{
-        "⠁ ",
-        "⠈ ",
-        " ⠁",
-        " ⠈",
-        " ⠐",
-        " ⠠",
-        " ⢀",
-        " ⡀",
-        "⢀ ",
-        "⡀ ",
-        "⠄ ",
-        "⠂ ",
+    //const frames = [12][]const u8{
+    //    "⠁ ",
+    //    "⠈ ",
+    //    " ⠁",
+    //    " ⠈",
+    //    " ⠐",
+    //    " ⠠",
+    //    " ⢀",
+    //    " ⡀",
+    //    "⢀ ",
+    //    "⡀ ",
+    //    "⠄ ",
+    //    "⠂ ",
+    //};
+
+    const frames = [9][]const u8{
+        "⠇ ",
+        "⠋ ",
+        "⠉⠁",
+        "⠈⠉",
+        " ⠙",
+        " ⠴",
+        "⠠⠤",
+        "⠤⠄",
+        "⠦ ",
     };
 
     while (true) {
         for (frames) |frame| {
             std.debug.print("\r{s}", .{frame});
-            std.time.sleep(80000000);
+            std.time.sleep(100000000);
         }
     }
 }
